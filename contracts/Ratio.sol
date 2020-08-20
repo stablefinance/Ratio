a6eff2091b9f97cebb3a1ca95ccdf41ab809b50a
engwarrior <lazaruscrypto@gmail.com>
2020-08-20 22:20:36 +0300

    Ratio initial supply

--- contracts/Ratio.sol
+++ contracts/Ratio.sol
@@ -9,6 +9,8 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 contract Ratio is ERC20, Ownable {
 
 	constructor() ERC20("Ratio", "RAT") public {
+		uint totalSupply = 100000000 * (10 ** 18);
+		_mint(msg.sender, totalSupply);
 	}
 
 	function mint(address account, uint amount) public onlyOwner {
